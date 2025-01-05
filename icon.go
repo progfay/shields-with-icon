630f310273c82fe5e61d624522736338adec4c4d
progfay <progfay@gmail.com>
2025-01-05 14:26:32 +0900

    deal with icon data file format changes
    - ref. https://github.com/simple-icons/simple-icons/pull/12415

--- icon.go
+++ icon.go
@@ -2,15 +2,59 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 )
 
+// [Brands] type of [simple-icons/simple-icons]
+//
+// [simple-icons/simple-icons]: https://github.com/simple-icons/simple-icons
+// [Brands]: https://github.com/simple-icons/simple-icons/blob/0ee651fd4dc50a48c35a96dc48a21984906a84e5/.jsonschema.json#L4
 type Icon struct {
 	Title  string `json:"title"`
 	Hex    string `json:"hex"`
 	Source string `json:"source"`
+}
+
+// ref. https://pkg.go.dev/encoding/json#example-Decoder.Decode-Stream
+func decodeIcons(r io.Reader) ([]Icon, error) {
+	// [simple-icons] type of [simple-icons/simple-icons]
+	//
+	// [simple-icons/simple-icons]: https://github.com/simple-icons/simple-icons
+	// [simple-icons]:  https://github.com/simple-icons/simple-icons/blob/0ee651fd4dc50a48c35a96dc48a21984906a84e5/.jsonschema.json
+	var icons []Icon
+	dec := json.NewDecoder(r)
+
+	// read open bracket
+	t, err := dec.Token()
+	if err != nil {
+		return nil, err
+	}
+	if t, ok := t.(json.Delim); !ok || t != '[' {
+		return nil, fmt.Errorf("first token is not '[': %T: %v", t, t)
+	}
+
+	for dec.More() {
+		var i Icon
+		err := dec.Decode(&i)
+		if err != nil {
+			return nil, err
+		}
+
+		icons = append(icons, i)
+	}
+
+	t, err = dec.Token()
+	if err != nil {
+		return nil, err
+	}
+	if t, ok := t.(json.Delim); !ok || t != ']' {
+		return nil, fmt.Errorf("last token is not ']': %T: %v", t, t)
+	}
+
+	return icons, nil
 }
 
 func getIcons() ([]Icon, error) {
@@ -21,14 +65,5 @@ func getIcons() ([]Icon, error) {
 	defer res.Body.Close()
 	defer io.Copy(ioutil.Discard, res.Body)
 
-	var simpleIcons struct {
-		Icons []Icon `json:"icons"`
-	}
-
-	err = json.NewDecoder(res.Body).Decode(&simpleIcons)
-	if err != nil {
-		return nil, err
-	}
-
-	return simpleIcons.Icons, nil
+	return decodeIcons(res.Body)
 }
